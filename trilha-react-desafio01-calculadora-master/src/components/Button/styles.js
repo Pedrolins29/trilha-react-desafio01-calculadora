99c2b2ee9bd4bd4bb2f5e36984a6b000bd255d13
Pedro Lins <101017619+Pedrolins29@users.noreply.github.com>
2022-12-10 17:54:01 -0300

    Add files via upload

---
+++ trilha-react-desafio01-calculadora-master/src/components/Button/styles.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const ButtonContainer = styled.button`
+    padding: 20px;
+    border: 1px solid #CDCDCD;
+    background-color: #3d3d3d;
+    color: #FFFFFF;
+    font-size: 24px;
+    font-weight: 700;
+    flex: 1;
+    border: 0;
+    
+    &:hover {
+        opacity: 0.6;
+    }
+`
